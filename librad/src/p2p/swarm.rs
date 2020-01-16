576c89bd0f809c988f76d09a008c5073e354c422
Kim Altintop <kim@monadic.xyz>
2020-01-16 18:10:46 +0100

    More. Async wut?

---
+++ librad/src/p2p/swarm.rs
@@ -0,0 +1,149 @@
+use std::time::Duration;
+use std::{
+    error::Error,
+    task::{Context, Poll},
+};
+
+use log::{debug, info};
+
+use async_std::{io, task};
+use futures::prelude::*;
+
+use libp2p::kad::record::store::MemoryStore;
+use libp2p::kad::{Kademlia, KademliaEvent};
+use libp2p::{
+    self, dns,
+    mdns::{Mdns, MdnsEvent},
+    mplex, secio,
+    swarm::NetworkBehaviourEventProcess,
+    tcp, yamux, Multiaddr, NetworkBehaviour, PeerId, Swarm, Transport, TransportError,
+};
+
+use crate::keys::device;
+
+#[derive(NetworkBehaviour)]
+pub struct RadBehaviour<TSubstream> {
+    kademlia: Kademlia<TSubstream, MemoryStore>,
+    mdns: Mdns<TSubstream>,
+}
+
+impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<MdnsEvent>
+    for RadBehaviour<TSubstream>
+{
+    fn inject_event(&mut self, event: MdnsEvent) {
+        if let MdnsEvent::Discovered(list) = event {
+            for (peer_id, multiaddr) in list {
+                debug!("Discovered peer via mDNS: {} @ {}", peer_id, multiaddr);
+                self.kademlia.add_address(&peer_id, multiaddr);
+            }
+        }
+    }
+}
+
+impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<KademliaEvent>
+    for RadBehaviour<TSubstream>
+{
+    // Called when `kademlia` produces an event.
+    fn inject_event(&mut self, message: KademliaEvent) {
+        debug!("Received KademliaEvent: {:?}", message);
+    }
+}
+
+pub async fn join(key: device::Key, listen_addr: Option<Multiaddr>) -> Result<(), Box<dyn Error>> {
+    let key = key.into_libp2p()?;
+    let peer_id = PeerId::from(key.public());
+
+    let transport = RadTransport::new()?.establish(key);
+    let mut swarm = {
+        let store = MemoryStore::new(peer_id.clone());
+        let kademlia = Kademlia::new(peer_id.clone(), store);
+        let mdns = Mdns::new()?;
+
+        let behaviour = RadBehaviour { kademlia, mdns };
+
+        Swarm::new(transport, behaviour, peer_id)
+    };
+
+    Swarm::listen_on(
+        &mut swarm,
+        listen_addr.unwrap_or_else(|| "/ip4/0.0.0.0/tcp/0".parse().unwrap()),
+    )?;
+
+    let mut listening = false;
+    task::block_on(future::poll_fn(move |cx: &mut Context| {
+        loop {
+            match swarm.poll_next_unpin(cx) {
+                Poll::Ready(Some(x)) => debug!("swarm.poll returned something: {:?}", x),
+                Poll::Ready(None) => return Poll::Ready(Ok(())),
+                Poll::Pending => {
+                    if !listening {
+                        if let Some(a) = Swarm::listeners(&swarm).next() {
+                            info!("Listening on {:?}", a);
+                            listening = true;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        Poll::Pending
+    }));
+
+    Ok(())
+}
+
+type TransportImpl = dns::DnsConfig<tcp::TcpConfig>;
+
+#[derive(Clone)]
+pub struct RadTransport(TransportImpl);
+
+impl RadTransport {
+    pub fn new() -> Result<Self, io::Error> {
+        dns::DnsConfig::new(tcp::TcpConfig::new().nodelay(true)).map(Self)
+    }
+
+    pub fn establish(
+        self,
+        keypair: libp2p::identity::Keypair,
+    ) -> impl Transport<
+        Output = (
+            PeerId,
+            impl libp2p::core::muxing::StreamMuxer<
+                    OutboundSubstream = impl Send,
+                    Substream = impl Send,
+                    Error = impl Into<io::Error>,
+                > + Send
+                + Sync,
+        ),
+        Error = impl std::error::Error + Send,
+        Listener = impl Send,
+        Dial = impl Send,
+        ListenerUpgrade = impl Send,
+    > + Clone {
+        self.upgrade(libp2p::core::upgrade::Version::V1)
+            .authenticate(secio::SecioConfig::new(keypair))
+            .multiplex(libp2p::core::upgrade::SelectUpgrade::new(
+                yamux::Config::default(),
+                mplex::MplexConfig::new(),
+            ))
+            .map(|(peer, muxer), _| (peer, libp2p::core::muxing::StreamMuxerBox::new(muxer)))
+            .timeout(Duration::from_secs(20))
+    }
+}
+
+impl Transport for RadTransport {
+    type Output = <TransportImpl as Transport>::Output;
+    type Error = <TransportImpl as Transport>::Error;
+    type Listener = <TransportImpl as Transport>::Listener;
+    type ListenerUpgrade = <TransportImpl as Transport>::ListenerUpgrade;
+    type Dial = <TransportImpl as Transport>::Dial;
+
+    fn listen_on(self, addr: Multiaddr) -> Result<Self::Listener, TransportError<Self::Error>> {
+        self.0.listen_on(addr)
+    }
+
+    fn dial(self, addr: Multiaddr) -> Result<Self::Dial, TransportError<Self::Error>> {
+        self.0.dial(addr)
+    }
+}
--- rad2/src/commands/daemon.rs
+++ rad2/src/commands/daemon.rs
@@ -1,101 +1,14 @@
 use std::error::Error;
 
-use futures::prelude::*;
-use libp2p::kad::record::store::MemoryStore;
-use libp2p::kad::{Kademlia, KademliaEvent};
-use libp2p::{
-    build_development_transport,
-    mdns::{Mdns, MdnsEvent},
-    swarm::NetworkBehaviourEventProcess,
-    tokio_io::{AsyncRead, AsyncWrite},
-    NetworkBehaviour, PeerId, Swarm,
-};
-use log::{debug, info};
 use structopt::StructOpt;
 
 use librad::keys::device;
+use librad::p2p::swarm;
 use librad::paths::Paths;
 
 #[derive(StructOpt)]
 pub struct Options {}
 
 pub fn run(_paths: Paths, _opts: Options, key: device::Key) -> Result<(), Box<dyn Error>> {
-    let local_key = key.into_libp2p()?;
-    let local_peer_id = PeerId::from(local_key.public());
-
-    info!("My Peer Id: {}", local_peer_id);
-
-    let transport = build_development_transport(local_key);
-
-    // We create a custom network behaviour that combines Kademlia and mDNS.
-    #[derive(NetworkBehaviour)]
-    struct MyBehaviour<TSubstream: AsyncRead + AsyncWrite> {
-        kademlia: Kademlia<TSubstream, MemoryStore>,
-        mdns: Mdns<TSubstream>,
-    }
-
-    impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<MdnsEvent>
-        for MyBehaviour<TSubstream>
-    {
-        // Called when `mdns` produces an event.
-        fn inject_event(&mut self, event: MdnsEvent) {
-            if let MdnsEvent::Discovered(list) = event {
-                for (peer_id, multiaddr) in list {
-                    debug!("Discovered peer via mDNS: {} @ {}", peer_id, multiaddr);
-                    self.kademlia.add_address(&peer_id, multiaddr);
-                }
-            }
-        }
-    }
-
-    impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<KademliaEvent>
-        for MyBehaviour<TSubstream>
-    {
-        // Called when `kademlia` produces an event.
-        fn inject_event(&mut self, message: KademliaEvent) {
-            info!("received KademliaEvent: {:?}", message);
-        }
-    }
-
-    // Create a swarm to manage peers and events.
-    let mut swarm = {
-        // Create a Kademlia behaviour.
-        let store = MemoryStore::new(local_peer_id.clone());
-        let kademlia = Kademlia::new(local_peer_id.clone(), store);
-
-        let behaviour = MyBehaviour {
-            kademlia,
-            mdns: Mdns::new().expect("Failed to create mDNS service"),
-        };
-
-        Swarm::new(transport, behaviour, local_peer_id.clone())
-    };
-
-    // Listen on all interfaces and whatever port the OS assigns.
-    Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
-
-    // Kick it off.
-    let mut listening = false;
-    tokio::run(futures::future::poll_fn(move || {
-        loop {
-            debug!("polling swarm");
-            match swarm.poll().expect("Error while polling swarm") {
-                Async::Ready(Some(x)) => debug!("swarm.poll returned something: {:?}", x),
-                Async::Ready(None) | Async::NotReady => {
-                    debug!("Ready(None) or NotReady");
-                    if !listening {
-                        if let Some(a) = Swarm::listeners(&swarm).next() {
-                            info!("Listening on {:?}", a);
-                            listening = true;
-                        }
-                    }
-                    break;
-                }
-            }
-        }
-
-        Ok(Async::NotReady)
-    }));
-
-    Ok(())
+    swarm::join(key, None)
 }
