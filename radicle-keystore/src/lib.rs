e5a2a61af5d896f4a5cc5421e8c96c46f574d691
Kim Altintop <kim@monadic.xyz>
2020-02-03 18:30:16 +0100

    Bit of docs

--- radicle-keystore/src/lib.rs
+++ radicle-keystore/src/lib.rs
@@ -49,6 +49,7 @@ pub trait HasMetadata {
     fn metadata(&self) -> Self::Metadata;
 }
 
+/// Abstraction over secure storage for private key material.
 pub trait Keystore {
     type Pinentry: Pinentry;
 
@@ -60,9 +61,26 @@ pub trait Keystore {
 
     type Error: std::error::Error;
 
+    /// Store secret key `key` in the keystore.
+    ///
+    /// The key may carry [`Self::Metadata`], which is stored alongside the key
+    /// material. The secret key material MUST be encrypted using a key
+    /// derived from the passphrase obtained via [`Self::Pinentry`]
+    ///
+    /// The public portion of the key (as obtained via
+    /// [`From<Self::SecretKey>`]) and the metadata may be stored in plain text.
+    ///
+    /// `put_key` MUST return an error if an equivalent key is already present
+    /// in the storage backend (i.e. key deletion / rotation is not provided
+    /// by this interface).
     fn put_key(&mut self, key: Self::SecretKey) -> Result<(), Self::Error>;
 
+    /// Retrieve both the secret and public parts of the stored key material.
+    ///
+    /// In order to decrypt the secret key, [`Self::Pinentry`] shall be invoked.
     fn get_key(&self) -> Result<Keypair<Self::PublicKey, Self::SecretKey>, Self::Error>;
 
+    /// Retrieve only the public part of the key material, along with any
+    /// metadata.
     fn show_key(&self) -> Result<(Self::PublicKey, Self::Metadata), Self::Error>;
 }
