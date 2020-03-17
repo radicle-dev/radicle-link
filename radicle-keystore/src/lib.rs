fe9f248a560bb31401f79c1bed587efffcad4e66
Kim Altintop <kim@monadic.xyz>
2020-03-17 19:30:37 +0100

    Add license headers

--- radicle-keystore/src/lib.rs
+++ radicle-keystore/src/lib.rs
@@ -1,3 +1,20 @@
+// This file is part of radicle-link
+// <https://github.com/radicle-dev/radicle-link>
+//
+// Copyright (C) 2019-2020 The Radicle Team <dev@radicle.xyz>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3 or
+// later as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 //! `radicle-keystore` aims to become the sole abstraction over storage of key
 //! material in the Radicle ecosystem.
 //!
