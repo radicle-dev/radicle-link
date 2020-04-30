00de03fb8bca2aa6bcc7512cd7bda8f825b72abf
FintanH <fintan.halpenny@gmail.com>
2020-04-30 16:55:36 +0100

    Document and test visibility

--- radicle-tracker/src/ops/visibility.rs
+++ radicle-tracker/src/ops/visibility.rs
@@ -18,16 +18,41 @@
 use crate::ops::{absurd, Apply};
 use std::convert::Infallible;
 
+/// The operation to apply to [`Visibility`].
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Op {}
 
+/// Determine whether something is visible.
+///
+/// This can mean hiding a deleted comment or a closed issue, for example.
+///
+/// This can be thought of as the logical OR operator for Booleans. If
+/// `Visibility` becomes `Hidden` it will always stay `Hidden`, in the same way
+/// `True || x == True`.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Visibility {
+    /// Here I am!
     Visible,
+    /// I'm hiding...
     Hidden,
 }
 
 impl Visibility {
+    /// I'm hiding away now.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use radicle_tracker::ops::visibility::Visibility;
+    ///
+    /// let mut visible = Visibility::Visible;
+    /// visible.hide();
+    /// assert_eq!(visible, Visibility::Hidden);
+    ///
+    /// // And it stays hidden.
+    /// visible.hide();
+    /// assert_eq!(visible, Visibility::Hidden);
+    /// ```
     pub fn hide(&mut self) -> Op {
         self.apply(Op {}).unwrap_or_else(absurd);
         Op {}
@@ -41,5 +66,65 @@ impl Apply for Visibility {
     fn apply(&mut self, _op: Self::Op) -> Result<(), Self::Error> {
         *self = Visibility::Hidden;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ops::absurd;
+    use proptest::prelude::*;
+
+    fn visibility_strategy() -> impl Strategy<Value = Visibility> {
+        prop_oneof![Just(Visibility::Visible), Just(Visibility::Hidden)]
+    }
+
+    proptest! {
+        #[test]
+        fn idempotent(visibility in visibility_strategy()) {
+            let mut left = visibility.clone();
+            left.apply(Op {}).unwrap_or_else(absurd);
+
+            let mut right = visibility.clone();
+            right.apply(Op {}).unwrap_or_else(absurd);
+            right.apply(Op {}).unwrap_or_else(absurd);
+
+            assert_eq!(left, right);
+        }
+
+        #[test]
+        fn commutative(visibility in visibility_strategy()) {
+            let x = Op {};
+            let y = Op {};
+
+            let mut left = visibility.clone();
+            left.apply(x.clone()).unwrap_or_else(absurd);
+            left.apply(y.clone()).unwrap_or_else(absurd);
+
+            let mut right = visibility.clone();
+            right.apply(y).unwrap_or_else(absurd);
+            right.apply(x).unwrap_or_else(absurd);
+
+            assert_eq!(left, right);
+        }
+
+        #[test]
+        fn associative(visibility in visibility_strategy()) {
+            let x = Op {};
+            let y = Op {};
+            let z = Op {};
+
+            let mut left = visibility.clone();
+            left.apply(x.clone()).unwrap_or_else(absurd);
+            left.apply(y.clone()).unwrap_or_else(absurd);
+            left.apply(z.clone()).unwrap_or_else(absurd);
+
+            let mut right = visibility.clone();
+            right.apply(y).unwrap_or_else(absurd);
+            right.apply(z).unwrap_or_else(absurd);
+            right.apply(x).unwrap_or_else(absurd);
+
+            assert_eq!(left, right);
+        }
     }
 }
