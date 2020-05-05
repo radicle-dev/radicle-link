fdba1ac7d33034d82e1dbc1d1218b1dd3c3fb3b5
FintanH <fintan.halpenny@gmail.com>
2020-05-05 14:35:48 +0100

    Clean up lib and testing module where we test folding over ops currently.

--- radicle-tracker/src/lib.rs
+++ radicle-tracker/src/lib.rs
@@ -95,15 +95,12 @@
 
 #![deny(missing_docs, unused_import_braces, unused_qualifications, warnings)]
 #![allow(clippy::new_without_default)]
-use std::hash::Hash;
 
 mod thread;
 pub use thread::{DataState, Error as ThreadError, Finger, Replies, ReplyTo, Thread};
 
 mod metadata;
 pub use metadata::*;
-
-use clock::{Clock, RadClock};
 
 /// TODO: Comment
 pub mod comment;
@@ -112,109 +109,5 @@ pub mod issue;
 /// TODO: Ops
 pub mod ops;
 
-/// An [`Issue`] that has been closed. The underlying issue cannot be mutated,
-/// and can we can only access the reference of this issue..
-#[derive(Debug, Clone)]
-pub struct ClosedIssue<Id, Cid, User: Eq + Hash>(Issue<Id, Cid, User>);
-
-impl<Id, Cid, User: Eq + Hash> ClosedIssue<Id, Cid, User> {
-    /// Reopen the underlying [`Issue`].
-    pub fn reopen(self) -> Issue<Id, Cid, User> {
-        self.0
-    }
-
-    /// Get a reference to the underlying [`Issue`].
-    /// This can be combined with any functionality that uses the `Issue` as a
-    /// reference. For example, we could follow this call with
-    /// [`Issue::thread`] and `clone` to view a thread's comments.
-    pub fn issue(&self) -> &Issue<Id, Cid, User> {
-        &self.0
-    }
-}
-
-/// An `Issue` is a conversation created by an original [`Issue::author`]. The
-/// issue is kicked off by providing a [`Title`] and an initial [`Comment`] that
-/// starts the main [`Thread`].
-///
-/// It also contains [`Metadata`] for which we would like to keep track of and
-/// enhance the experience of the conversation.
-#[derive(Debug, Clone)]
-pub struct Issue<Id, Cid, User: Eq + Hash> {
-    identifier: Id,
-    author: User,
-    title: Title,
-    thread: Thread<Comment<Cid, User>>,
-    meta: Metadata<User>,
-    timestamp: RadClock,
-}
-
-impl<Id, Cid, User: Eq + Hash> Issue<Id, Cid, User> {
-    /// Create a new `Issue`.
-    pub fn new(identifier: Id, comment_id: Cid, author: User, title: Title, content: String) -> Self
-    where
-        User: Clone + Eq,
-    {
-        let timestamp = RadClock::current_time();
-        Self::new_with_timestamp(identifier, comment_id, author, title, content, timestamp)
-    }
-
-    /// Create a new `Issue` with a supplied `timestamp`.
-    pub fn new_with_timestamp(
-        identifier: Id,
-        comment_id: Cid,
-        author: User,
-        title: Title,
-        content: String,
-        timestamp: RadClock,
-    ) -> Self
-    where
-        User: Clone + Eq,
-    {
-        let comment = Comment::new_with_timestamp(comment_id, author.clone(), content, timestamp);
-
-        Issue {
-            identifier,
-            author,
-            title,
-            thread: Thread::new(comment),
-            meta: Metadata::new(),
-            timestamp,
-        }
-    }
-
-    /// Close an [`Issue`] and get back a [`ClosedIssue`]. This limits the
-    /// functionality on the original `Issue`.
-    pub fn close(self) -> ClosedIssue<Id, Cid, User> {
-        ClosedIssue(self)
-    }
-
-    /// Get a reference to the author (`User`) of this issue.
-    pub fn author(&self) -> &User {
-        &self.author
-    }
-
-    /// Get a reference to the [`Title`] of this issue.
-    pub fn title(&self) -> &Title {
-        &self.title
-    }
-
-    /// Get a reference to the [`Thread`] of this issue.
-    pub fn thread(&self) -> &Thread<Comment<Cid, User>> {
-        &self.thread
-    }
-
-    /// Get a mutable reference to the [`Thread`] of this issue.
-    pub fn thread_mut(&mut self) -> &mut Thread<Comment<Cid, User>> {
-        &mut self.thread
-    }
-
-    /// Get a reference to the [`Metadata`] of this issue.
-    pub fn meta(&self) -> &Metadata<User> {
-        &self.meta
-    }
-
-    /// Get a mutable reference to the [`Metadata`] of this issue.
-    pub fn meta_mut(&mut self) -> &mut Metadata<User> {
-        &mut self.meta
-    }
-}
+#[cfg(test)]
+pub mod test;
---
+++ radicle-tracker/src/test.rs
@@ -0,0 +1,139 @@
+// This file is part of radicle-link
+// <https://github.com/radicle-dev/radicle-link>
+//
+// Copyright (C) 2019-2020 The Radicle Team <dev@radicle.xyz>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3 or
+// later as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::{
+    comment::Comment,
+    issue::Issue,
+    metadata::{Label, Reaction, Title},
+    ops::thread::{AppendTo, Finger, ReplyFinger},
+};
+use pretty_assertions::assert_eq;
+
+#[test]
+fn folding_ops() {
+    let mut global_comment_id = 0;
+    let mut new_comment_id = || {
+        let new_id = global_comment_id;
+        global_comment_id += 1;
+        new_id
+    };
+
+    let initial_comment_id = new_comment_id();
+
+    let mut issue = Issue::new(
+        1,
+        initial_comment_id,
+        "fintohaps",
+        Title::from("Issue Ops"),
+        "This is how we create issues in radicle".to_string(),
+    );
+
+    let mut expected_issue = issue.clone();
+    let title_change = expected_issue.replace_title(Title::from("Issue Ops - Origami"));
+    let assign_massi = expected_issue.assign("mmassi");
+    let label_collab = expected_issue.label(Label::from("collab"));
+    let replace_description = expected_issue
+        .with_comments(|thread| {
+            thread.edit(Finger::Root, |comment| {
+                Ok(comment
+                    .replace_content(
+                        "fintohaps",
+                        "This is how we create issues in radicle: by folding over operations."
+                            .to_string(),
+                    )
+                    .unwrap())
+            })
+        })
+        .expect("Failed to edit comment");
+    let kim_comment = expected_issue
+        .with_comments(|thread| {
+            thread.append(
+                AppendTo::Main,
+                Comment::new(
+                    new_comment_id(),
+                    "kim",
+                    "Are these operations CRDTs?".to_string(),
+                ),
+            )
+        })
+        .expect("kim failed to append comment");
+    let finto_reply_to_kim = expected_issue.with_comments(|thread| {
+        thread.append(AppendTo::Thread(0),
+        Comment::new(
+            new_comment_id(),
+            "fintohaps",
+            "Not quite, they look similar but they hold less state and causality happens locally.".to_string()))
+    }).expect("fintohaps reply to thread failed");
+    let xla_comment = expected_issue
+        .with_comments(|thread| {
+            thread.append(
+                AppendTo::Main,
+                Comment::new(
+                    new_comment_id(),
+                    "xla",
+                    "Let's integrate! :rocket:".to_string(),
+                ),
+            )
+        })
+        .expect("xla comment failed");
+    let finto_reaction_to_xla = expected_issue
+        .with_comments(|thread| {
+            thread.edit(Finger::Reply(ReplyFinger::Main(1)), |comment| {
+                Ok(comment.react(Reaction {
+                    user: "fintohaps",
+                    value: ":heart".to_string(),
+                }))
+            })
+        })
+        .expect("fintohaps failed to react to xla");
+    let finto_edit_comment = expected_issue
+        .with_comments(|thread| {
+            thread.edit(
+                // Note here that the reply is 1, because { main: 0, reply: 0 } represents
+                // kim's comment.
+                Finger::Reply(ReplyFinger::Thread { main: 0, reply: 1 }),
+                |comment| {
+                    Ok(comment
+                    .replace_content(
+                        "fintohaps",
+                        "They look similar but they hold less state and causality happens locally."
+                            .to_string(),
+                    )
+                    .expect("only fintohaps can edit his own comment"))
+                },
+            )
+        })
+        .expect("fintohaps failed to edit his comment");
+
+    let ops = vec![
+        title_change,
+        assign_massi,
+        label_collab,
+        replace_description,
+        kim_comment,
+        xla_comment,
+        finto_reply_to_kim,
+        finto_reaction_to_xla,
+        finto_edit_comment,
+    ];
+
+    issue
+        .fold_issue(ops.into_iter())
+        .expect("Folding of ops failed");
+
+    assert_eq!(expected_issue, issue);
+}
