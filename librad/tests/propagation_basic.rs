131a7d1365a66c52696523d4f09c99e07a68f82b
Kim Altintop <kim@monadic.xyz>
2020-05-18 17:30:14 +0200

    Lol

--- librad/tests/propagation_basic.rs
+++ librad/tests/propagation_basic.rs
@@ -12,7 +12,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a coopy of the GNU General Public License
+// You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use std::{future::Future, time::Duration};
