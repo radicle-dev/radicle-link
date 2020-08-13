f42c0ce3da85a4ff8a6594545beb7c1f8940e89e
Alexis Sellier <self@cloudhead.io>
2020-08-13 15:54:53 +0200

    Implement main function

--- radicle-seed/src/main.rs
+++ radicle-seed/src/main.rs
@@ -1,3 +1,10 @@
+use radicle_seed::{Node, NodeConfig};
+
+use futures::executor;
+
 fn main() {
-    todo!();
+    let config = NodeConfig::default();
+    let node = Node::new(config).unwrap();
+
+    executor::block_on(node.run()).unwrap();
 }
