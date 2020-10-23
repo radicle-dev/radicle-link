2d9dbe155411817f4429fa2ccb609689e7fbd38f
Alexis Sellier <self@cloudhead.io>
2020-10-23 13:51:43 +0200

    Format

--- seed/src/event.rs
+++ seed/src/event.rs
@@ -14,6 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use std::net::SocketAddr;
 
 use librad::{meta::project::ProjectInfo, net::peer::PeerApi, peer::PeerId, uri::RadUrn};
--- seed/src/handle.rs
+++ seed/src/handle.rs
@@ -14,6 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use std::{collections::HashMap, net::SocketAddr};
 
 use futures::{channel::mpsc as chan, sink::SinkExt as _, stream::StreamExt as _};
