0a57f36a1b891f334473a23accd5e28a2d985a4a
Kim Altintop <kim@monadic.xyz>
2020-10-28 15:49:26 +0100

    fixup! Make fetcher construction a trait

--- librad/src/git/fetch.rs
+++ librad/src/git/fetch.rs
@@ -15,6 +15,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+// clippy doesn't know GATs yet
+#![allow(clippy::needless_lifetimes)]
+
 use std::{
     collections::{BTreeMap, BTreeSet},
     convert::TryFrom,
