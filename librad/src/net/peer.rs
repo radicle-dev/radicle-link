4d88f31bc23a790930fbfc8dd89666af1fdc1745
FintanH <fintan.halpenny@gmail.com>
2020-11-04 12:05:25 +0000

    Lock up and lock down

--- librad/src/net/peer.rs
+++ librad/src/net/peer.rs
@@ -524,9 +524,11 @@ where
         let span = tracing::info_span!("Peer::LocalStorage::put");
         let _guard = span.enter();
 
-        let git = self.inner.get().await.unwrap();
-        if git.peer_id() == provider {
-            return PutResult::Stale;
+        {
+            let git = self.inner.get().await.unwrap();
+            if git.peer_id() == provider {
+                return PutResult::Stale;
+            }
         }
 
         match has.urn.proto {
@@ -605,13 +607,16 @@ where
     async fn ask(&self, want: Self::Update) -> bool {
         let span = tracing::info_span!("Peer::LocalStorage::ask");
         let _guard = span.enter();
-        let git = self.inner.get().await.unwrap();
+        let local_peer_id = {
+            let git = self.inner.get().await.unwrap();
+            git.peer_id()
+        };
 
         match want.urn.proto {
             uri::Protocol::Git => {
                 self.git_has(
                     match want.origin {
-                        Some(origin) if origin == git.peer_id() => Either::Left(want.urn),
+                        Some(origin) if origin == local_peer_id => Either::Left(want.urn),
                         Some(origin) => Either::Right(Originates {
                             from: origin,
                             value: want.urn,
