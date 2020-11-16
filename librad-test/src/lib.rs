2292a635e98723f7142e9c9f1950b6a1765729b5
Kim Altintop <kim@monadic.xyz>
2020-11-16 15:59:36 +0100

    Remove unused result_flattening feature

--- librad-test/src/lib.rs
+++ librad-test/src/lib.rs
@@ -15,8 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#![feature(result_flattening)]
-
 #[macro_use]
 extern crate lazy_static;
 
