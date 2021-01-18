ab66515d46e7ce17219244b68dd1a326d95ce819
Fintan Halpenny <fintan.halpenny@gmail.com>
2021-01-18 16:41:12 +0000

    Adding test infra for git storage API
    This change is kicking off the introduction of testing Storage combined
    with git::identities.
    
    The initial test is attempting to create a project with a single,
    anonymous delegation. We then check that we can use `project::get` and
    `any::get` to get back the created project.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

---
+++ librad/src/git/tests/common.rs
@@ -0,0 +1,43 @@
+// Copyright © 2019-2020 The Radicle Foundation <hello@radicle.foundation>
+//
+// This file is part of radicle-link, distributed under the GPLv3 with Radicle
+// Linking Exception. For full terms see the included LICENSE file.
+
+use std::io;
+
+use librad_test::tempdir::WithTmpDir;
+
+use crate::{
+    git::{
+        identities::{self, local},
+        storage::Storage,
+    },
+    identities::payload,
+    keys::SecretKey,
+    paths::Paths,
+};
+
+pub type TmpStorage = WithTmpDir<Storage>;
+
+pub fn storage(signer: SecretKey) -> anyhow::Result<TmpStorage> {
+    Ok(WithTmpDir::new(|path| {
+        let paths = Paths::from_root(path)?;
+        let storage = Storage::open_or_init(&paths, signer)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        Ok::<_, io::Error>(storage)
+    })?)
+}
+
+pub fn dylan(storage: &Storage, key: &SecretKey) -> anyhow::Result<local::LocalIdentity> {
+    let dylan = identities::person::create(
+        storage,
+        payload::Person {
+            name: "dylan".into(),
+        },
+        Some(key.public()).into_iter().collect(),
+    )?;
+    Ok(
+        local::load(&storage, dylan.urn())?
+            .ok_or_else(|| anyhow::anyhow!("where did dylan go?"))?,
+    )
+}
