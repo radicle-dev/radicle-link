d39a84088fa1d44f87fdbc2cec1f2a7a47042a19
Fintan Halpenny <fintan.halpenny@gmail.com>
2021-01-31 15:48:02 +0000

    Introduce radicle-copy
    radicle-copy is the API for creating projects in the monorepo _as well_
    as a working copy to mirror it. It allows the creation of a fresh new
    project or a project based off of an existing git repository.
    
    A part of working copy management is also generating and maintaining
    include files that persist the tracked remotes.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

---
+++ copy/src/lib.rs
@@ -0,0 +1,83 @@
+// Copyright © 2019-2020 The Radicle Foundation <hello@radicle.foundation>
+//
+// This file is part of radicle-link, distributed under the GPLv3 with Radicle
+// Linking Exception. For full terms see the included LICENSE file.
+
+#![feature(never_type)]
+
+use librad::{
+    self,
+    git::{
+        identities::{self, local::LocalIdentity, Project},
+        local::{
+            transport::{self, CanOpenStorage},
+            url::LocalUrl,
+        },
+        storage::Storage,
+    },
+    identities::{delegation::Indirect, payload},
+    paths::Paths,
+    signer::BoxedSigner,
+};
+
+pub mod error;
+use error::Error;
+
+pub mod existing;
+pub mod include;
+pub mod new;
+
+mod git;
+
+// FIXME: seal
+pub trait CreateRepo {
+    type Error;
+
+    fn init<F>(self, url: LocalUrl, transport: F) -> Result<git2::Repository, Self::Error>
+    where
+        F: CanOpenStorage + 'static;
+}
+
+// FIXME: seal
+pub trait AsPayload {
+    fn as_payload(&self) -> payload::Project;
+}
+
+/// Create a [`Project`] and its working copy from the provided `payload`. The
+/// `payload` must implement [`CreateRepo`] and [`AsPayload`], the only types of
+/// which are:
+///   * [`exisiting::Existing<exisiting::Valid>`]
+///   * [`new::New<new::Valid>`]
+///
+/// This means that to provide a `payload` you must have a valid construction so
+/// that we don't end up with bad state.
+pub fn init<P>(
+    paths: Paths,
+    signer: BoxedSigner,
+    storage: &Storage,
+    whoami: LocalIdentity,
+    payload: P,
+) -> Result<Project, Error>
+where
+    P: AsPayload + CreateRepo,
+    P::Error: Into<error::Error>,
+{
+    let project = identities::project::create::<payload::Project>(
+        storage,
+        whoami.clone(),
+        payload.as_payload(),
+        Indirect::from(whoami.into_inner().into_inner()),
+    )?;
+
+    let transport = transport::Settings {
+        paths: paths.clone(),
+        signer,
+    };
+    let url = LocalUrl::from(project.urn());
+    let repo = payload.init(url, transport).map_err(|err| err.into())?;
+
+    let path = include::update(storage, &paths, &project)?;
+    librad::git::include::set_include_path(&repo, path).map_err(include::Error::from)?;
+
+    Ok(project)
+}
