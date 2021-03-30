694dfaf582c665c2e32cbf9008b946d9ead61f4d
Fintan Halpenny <fintan.halpenny@gmail.com>
2021-03-30 15:57:50 +0100

    daemon: migrating the radicle-upstream/coco code
    We're moving the code that was living in radicle-upstream/coco to start
    as the foundation of a daemon.
    
    The one caveat is that coco/source.rs will not be present as its
    migration path is towards radicle-surf.
    
    Everything else is a verbatim move of the coco files where the only
    changes were for imports. These were mostly using `librad::git_ext` as
    opposed to `radicle_git_ext` and the removal of `radicle_surf::git2`.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

---
+++ daemon/src/lib.rs
@@ -0,0 +1,53 @@
+//! Abstractions and utilities to run and interact with link and surf.
+
+#![warn(
+    clippy::all,
+    clippy::cargo,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::unwrap_used,
+    missing_docs,
+    unused_import_braces,
+    unused_qualifications
+)]
+#![allow(
+    clippy::clone_on_ref_ptr,
+    clippy::expect_used,
+    clippy::implicit_return,
+    clippy::integer_arithmetic,
+    clippy::missing_inline_in_public_items,
+    clippy::multiple_crate_versions,
+    clippy::multiple_inherent_impl,
+    clippy::similar_names,
+    clippy::too_many_lines
+)]
+#![feature(duration_zero, hash_set_entry, never_type, or_patterns)]
+
+pub use librad::{
+    git::{self, identities::local::LocalIdentity, include, local::url::LocalUrl, Urn},
+    identities::{self, Person, Project},
+    keys,
+    net::{self, discovery},
+    paths::Paths,
+    peer::PeerId,
+    profile, signer,
+};
+
+pub use librad::git_ext;
+
+pub use radicle_git_helpers::remote_helper;
+
+pub mod config;
+pub mod control;
+pub mod convert;
+pub mod git_helper;
+mod identifier;
+pub use identifier::Identifier;
+pub mod keystore;
+pub mod peer;
+pub use peer::{Control as PeerControl, Event as PeerEvent, Peer, RunConfig, Status as PeerStatus};
+pub mod project;
+pub mod request;
+pub mod state;
+
+pub mod seed;
