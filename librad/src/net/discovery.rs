radicle-dev/radicle-link

commit 98e310d31404bf41475e49ba818b5aefced416e9

Migrate to Tokio EINS
Brings all dependencies in and around tokio up-to-date.

Signed-off-by: Alexander Simmerl <a.simmerl@gmail.com>

librad/Cargo.toml
@@ -10,6 +10,7 @@ default = []
 disco-mdns = ["mdns", "madness"]
 
 [dependencies]
+async-stream = "0.3"
 async-trait = "0.1"
 bit-vec = "0.6"
 bs58 = "0.3"
@@ -22,7 +23,7 @@ globset = "0.4"
 governor = ">=0.3.2"
 lazy_static = "1"
 libc = "0.2"
-mdns = { version = "1", optional = true }
+mdns = { version = "3", optional = true }
 multibase = "0.9"
 multihash = "0.11"
 nom = "5"
@@ -83,7 +84,7 @@ features = []
 
 [dependencies.madness]
 git = "https://github.com/meilisearch/madness"
-rev = "cb12159a06f3698d0ba4ea078e95f8a915a9a9d3"
+rev = "12a7386952923990179fe400e0cd932cbdd90dfc"
 optional = true
 
 [dependencies.minicbor]
@@ -92,7 +93,7 @@ features = ["std", "derive"]
 
 [dependencies.quinn]
 git = "https://github.com/quinn-rs/quinn"
-rev = "47232c83d1615e96b4a8480ec25fbf7fcf6771f5"
+rev = "05bb6cdd5f53484f538a20da37407ef13faf42e0"
 default-features = false
 features = ["tls-rustls"]
 
@@ -107,20 +108,24 @@ path = "../std-ext"
 
 # Note: this MUST always match the exact patch version `quinn` uses
 [dependencies.rustls]
-git = "https://github.com/ctz/rustls"
-rev = "fee894f7e030"
+version  = "0.19"
 features = ["logging", "dangerous_configuration"]
 
 [dependencies.serde]
 version = "1.0"
 features = ["derive"]
 
+# 0.1.12 breaks our builds
+# See: https://github.com/tokio-rs/tracing/issues/1227
+[dependencies.tracing-attributes]
+version = "<= 0.1.11"
+
 [dependencies.tokio]
-version = "0.2"
+version = "1.1"
 features = ["full"]
 
 [dependencies.tokio-util]
-version = "0.3"
+version = "0.6"
 features = ["compat"]
 
 [dependencies.url]

e2e/Cargo.toml
@@ -22,4 +22,4 @@ path = "../librad"
 features = ["disco-mdns"]
 
 [dependencies.tokio]
-version = "0.2"
+version = "1.1"

librad-test/Cargo.toml
@@ -18,7 +18,7 @@ pretty_assertions = "0"
 serde = "1"
 serde_json = "1"
 tempfile = "3"
-tokio = "0.2"
+tokio = "1.1"
 tracing = ">= 0.1"
 tracing-subscriber = ">= 0.2"
 

seed/Cargo.toml
@@ -11,5 +11,5 @@ futures = "0.3"
 librad = { path = "../librad" }
 radicle-keystore = "0"
 thiserror = "1"
-tokio = { version = "0.2", features = ["time"] }
+tokio = { version = "1.1", features = ["time"] }
 tracing = "0.1"

ci/build-test
@@ -2,7 +2,7 @@
 set -eou pipefail
 
 echo '--- Build'
-cargo build --workspace --all-features
+cargo build --verbose --workspace --all-features
 
 echo '--- Test'
 GIT_TRACE2=1 RUST_LOG=librad=trace cargo test --workspace --all-features

e2e/src/bin/ephemeral-peer.rs
@@ -100,10 +100,10 @@ async fn emit_stats(peer: Peer<SecretKey>) -> anyhow::Result<!> {
         )
     };
 
-    let mut sock = tokio::net::UdpSocket::bind("127.0.0.1:0").await?;
+    let sock = tokio::net::UdpSocket::bind("127.0.0.1:0").await?;
     sock.connect("127.0.0.1:9109").await?;
     loop {
-        tokio::time::delay_for(Duration::from_secs(10)).await;
+        tokio::time::sleep(Duration::from_secs(10)).await;
         let stats = peer.stats().await;
         tracing::info!("stats: {:?}", stats);
 

librad-test/src/rad/testnet.rs
@@ -210,7 +210,7 @@ where
 pub async fn wait_converged<E>(events: E, min_connected: usize)
 where
     E: IntoIterator,
-    E::Item: futures::Stream<Item = Result<protocol::event::Upstream, protocol::RecvError>> + Unpin,
+    E::Item: futures::Stream<Item = Result<protocol::event::Upstream, protocol::RecvError>> + Send,
 {
     if min_connected < 2 {
         return;
@@ -224,6 +224,7 @@ where
                     future::ok(!matches!(evt, protocol::event::Upstream::Membership(_)))
                 })
                 .map_ok(drop)
+                .boxed()
                 .into_future()
                 .map(|(x, _)| x)
         })

librad/src/git/p2p/server.rs
@@ -27,7 +27,7 @@ use futures::{
 use git2::transport::Service;
 use git_ext::{into_io_err, RefLike, References, UPLOAD_PACK_HEADER};
 use tokio::process::{self, Command};
-use tokio_util::compat::{Tokio02AsyncReadCompatExt, Tokio02AsyncWriteCompatExt};
+use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
 
 use super::{
     super::{
@@ -187,8 +187,8 @@ impl UploadPack {
                 let mut stdout = child.stdout.take().unwrap().compat();
 
                 send.write_all(UPLOAD_PACK_HEADER).await?;
-                futures::try_join!(futures::io::copy(&mut stdout, &mut send), child).and_then(
-                    |(_, status)| {
+                futures::try_join!(futures::io::copy(&mut stdout, &mut send), child.wait())
+                    .and_then(|(_, status)| {
                         if !status.success() {
                             Err(io::Error::new(
                                 io::ErrorKind::Other,
@@ -197,8 +197,7 @@ impl UploadPack {
                         } else {
                             Ok(())
                         }
-                    },
-                )
+                    })
             },
 
             Self::UploadPack(mut child) => {
@@ -208,7 +207,7 @@ impl UploadPack {
                 futures::try_join!(
                     futures::io::copy(&mut recv, &mut stdin),
                     futures::io::copy(&mut stdout, &mut send),
-                    child,
+                    child.wait(),
                 )
                 .and_then(|(_, _, status)| {
                     if !status.success() {

commit 6e656feefdc85ab4c9aabb57f486de8b6bd5aad4

net: remove mDNS discovery
mDNS discovery appears to be broken since
98e310d31404bf41475e49ba818b5aefced416e9 (Migrate to Tokio EINS), and no
quick fix could be found. Since this code is de-facto not covered by
tests, and only mildly useful, we remove it until a more robust
implementation is found or created.

Signed-off-by: Kim Altintop <kim@monadic.xyz>

deny.toml
@@ -203,6 +203,5 @@ unknown-git = "deny"
 allow-registry = ["https://github.com/rust-lang/crates.io-index"]
 # List of URLs for allowed Git repositories
 allow-git = [
-    "https://github.com/meilisearch/madness",
     "https://github.com/ZcashFoundation/ed25519-zebra",
 ]

librad/Cargo.toml
@@ -5,10 +5,6 @@ authors = ["The Radicle Team <dev@radicle.xyz>"]
 edition = "2018"
 license = "GPL-3.0-or-later"
 
-[features]
-default = []
-disco-mdns = ["mdns", "madness"]
-
 [dependencies]
 async-stream = "0.3"
 async-trait = "0.1"
@@ -24,7 +20,6 @@ globset = "0.4"
 governor = ">=0.3.2"
 lazy_static = "1"
 libc = "0.2"
-mdns = { version = "3", optional = true }
 multibase = "0.9"
 multihash = "0.11"
 nom = "5"
@@ -88,11 +83,6 @@ features = []
 version = "0.12"
 default-features = false
 features = []
-
-[dependencies.madness]
-git = "https://github.com/meilisearch/madness"
-rev = "12a7386952923990179fe400e0cd932cbdd90dfc"
-optional = true
 
 [dependencies.minicbor]
 version = ">= 0.6, 0"

librad/src/net/discovery.rs
@@ -14,11 +14,6 @@ use std::{
 
 use crate::peer::PeerId;
 
-#[cfg(feature = "disco-mdns")]
-mod mdns;
-#[cfg(feature = "disco-mdns")]
-pub use self::mdns::Mdns;
-
 pub trait Discovery {
     type Addr;
     type Stream: futures::Stream<Item = (PeerId, Vec<Self::Addr>)> + Send;

librad/src/net/discovery/mdns.rs
@@ -1,509 +1,0 @@
-// Copyright © 2019-2020 The Radicle Foundation <hello@radicle.foundation>
-//
-// This file is part of radicle-link, distributed under the GPLv3 with Radicle
-// Linking Exception. For full terms see the included LICENSE file.
-
-use std::{
-    cmp,
-    collections::{BTreeMap, BTreeSet, BinaryHeap},
-    hash::{Hash, Hasher as _},
-    net::{IpAddr, SocketAddr},
-    pin::Pin,
-    sync::{Arc, RwLock, Weak},
-    task::{Context, Poll},
-    time::{Duration, Instant},
-};
-
-use futures::{channel::mpsc, stream::StreamExt as _, SinkExt as _};
-use madness::{
-    dns::{PacketBuilder, RData, ResourceRecord},
-    service::{Query, ServiceDiscovery},
-    MdnsService,
-    Packet,
-};
-use rustc_hash::FxHashMap;
-
-use super::Discovery;
-use crate::peer::PeerId;
-
-const SERVICE_NAME: &str = "_radicle-link._udp.local";
-
-pub struct Mdns {
-    _query: ServiceDiscovery,
-    _cache: Arc<RwLock<Discoveries<PeerId, u64>>>,
-    chan: mpsc::Receiver<(PeerId, Vec<SocketAddr>)>,
-}
-
-impl Mdns {
-    pub fn new(
-        local_peer: PeerId,
-        listen_addrs: Vec<SocketAddr>,
-        query_interval: Duration,
-        cache_ttl: Duration,
-    ) -> Result<Self, Box<dyn std::error::Error>> {
-        let mut srv = MdnsService::new(true)?;
-        srv.register(SERVICE_NAME);
-        let query = srv.discover(SERVICE_NAME, query_interval);
-
-        let ptr = format!("{}.{}", local_peer, SERVICE_NAME);
-        let cname = format!("{}.radicle.local", local_peer);
-        let cache = Arc::new(RwLock::new(Discoveries::new(cmp::max(
-            cache_ttl,
-            query_interval,
-        ))));
-        let (tx, rx) = mpsc::channel(64);
-
-        tokio::spawn(run(
-            srv,
-            ptr,
-            cname,
-            listen_addrs,
-            Arc::downgrade(&cache),
-            tx,
-        ));
-
-        Ok(Self {
-            _query: query,
-            _cache: cache,
-            chan: rx,
-        })
-    }
-
-    #[cfg(test)]
-    pub fn is_pending(&mut self) -> bool {
-        use futures::Stream as _;
-
-        let waker = futures::task::noop_waker();
-        matches!(
-            Pin::new(&mut self.chan).poll_next(&mut Context::from_waker(&waker)),
-            Poll::Pending
-        )
-    }
-}
-
-#[tracing::instrument(skip(srv, ptr, cname, listen_addrs, cache, tx))]
-async fn run(
-    mut srv: MdnsService,
-    ptr: String,
-    cname: String,
-    listen_addrs: Vec<SocketAddr>,
-    cache: Weak<RwLock<Discoveries<PeerId, u64>>>,
-    mut tx: mpsc::Sender<(PeerId, Vec<SocketAddr>)>,
-) {
-    use std::collections::hash_map::DefaultHasher;
-
-    tracing::info!("starting mDNS service discovery");
-
-    loop {
-        let packet = srv.next().await;
-        tracing::trace!(packet = ?packet, "received");
-        match Weak::upgrade(&cache) {
-            None => {
-                tracing::info!("stopping the madness");
-                return;
-            },
-
-            Some(cache) => match packet {
-                Packet::Query(queries) => {
-                    for query in queries {
-                        respond(&ptr, &cname, &listen_addrs, &mut srv, query)
-                    }
-                },
-                Packet::Response(resp) => {
-                    for (peer, addrs) in discover(&cname, resp) {
-                        let addrs_hash = {
-                            let mut hasher = DefaultHasher::new();
-                            addrs.hash(&mut hasher);
-                            hasher.finish()
-                        };
-                        let discovered = cache.write().unwrap().insert(peer, addrs_hash);
-                        match discovered {
-                            Discovered::New((peer, _)) | Discovered::Refreshed((peer, _)) => {
-                                tracing::debug!("{} is new or refreshed", peer);
-                                tx.send((peer, addrs)).await.ok();
-                            },
-
-                            Discovered::Known((peer, _)) => {
-                                tracing::trace!("ignoring known peer {}", peer);
-                            },
-                        }
-                    }
-                },
-            },
-        }
-    }
-}
-
-impl futures::stream::Stream for Mdns {
-    type Item = (PeerId, Vec<SocketAddr>);
-
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
-        self.chan.poll_next_unpin(cx)
-    }
-}
-
-impl Discovery for Mdns {
-    type Addr = SocketAddr;
-    type Stream = Self;
-
-    fn discover(self) -> Self::Stream {
-        self
-    }
-}
-
-#[derive(Debug)]
-#[must_use]
-enum Discovered<T> {
-    Known(T),
-    New(T),
-    Refreshed(T),
-}
-
-/// A simple key-value cache with time-based eviction.
-///
-/// The eviction policy is maintained on insertion with the following
-/// properties:
-///
-/// * Entries with a timestamp smaller than the current time are removed from
-///   the cache.
-/// * If the key is not in the cache, it is inserted along with the current
-///   time. The cache entry is considered [`Discovered::New`].
-/// * If the key is found, but the associated value is different from the one
-///   given, then the value is replaced by the given one, along with a timestamp
-///   equal to the current time. The cache entry is considered
-///   [`Discovered::Refreshed`].
-/// * If the key is found, and the associated value is equal to the one given,
-///   the entry is considered [`Discovered::Known`], and the cache is left
-///   unchanged.
-struct Discoveries<K, V> {
-    all: FxHashMap<K, Timestamped<V>>,
-    hip: BinaryHeap<Oldest<K>>,
-    ttl: Duration,
-}
-
-impl<K, V> Discoveries<K, V>
-where
-    K: Clone + Eq + Hash,
-    V: Clone + PartialEq,
-{
-    pub fn new(ttl: Duration) -> Self {
-        Self {
-            all: Default::default(),
-            hip: Default::default(),
-            ttl,
-        }
-    }
-
-    pub fn insert(&mut self, k: K, v: V) -> Discovered<(K, V)> {
-        use std::collections::hash_map::Entry::*;
-        use Discovered::*;
-
-        let now = Instant::now();
-        self.evict(now - self.ttl);
-        match self.all.entry(k) {
-            Occupied(mut entry) => {
-                let key = entry.key().clone();
-                let prev = entry.get_mut();
-                if prev.inner != v {
-                    *prev = Timestamped {
-                        timestamp: now,
-                        inner: v.clone(),
-                    };
-                    Refreshed((key, v))
-                } else {
-                    Known((key, v))
-                }
-            },
-
-            Vacant(entry) => {
-                let key = entry.key().clone();
-                entry.insert(Timestamped {
-                    timestamp: now,
-                    inner: v.clone(),
-                });
-                self.hip.push(Oldest {
-                    inner: Timestamped {
-                        timestamp: now,
-                        inner: key.clone(),
-                    },
-                });
-                New((key, v))
-            },
-        }
-    }
-
-    fn evict(&mut self, deadline: Instant) {
-        while let Some(Oldest {
-            inner: Timestamped { timestamp, .. },
-        }) = self.hip.peek()
-        {
-            if *timestamp > deadline {
-                return;
-            }
-
-            let key: K = self
-                .hip
-                .pop()
-                .expect("I peeked, therefore I pop")
-                .inner
-                .inner;
-            let value_timestamp = self.all[&key].timestamp;
-            if value_timestamp > deadline {
-                self.hip.push(Oldest {
-                    inner: Timestamped {
-                        timestamp: value_timestamp,
-                        inner: key,
-                    },
-                });
-            } else {
-                self.all.remove(&key);
-            }
-        }
-    }
-}
-
-struct Timestamped<T> {
-    timestamp: Instant,
-    inner: T,
-}
-
-/// Compares [`Timestamped`] values by time, such that the smaller timestamp is
-/// [`cmp::Ordering::Greater`].
-struct Oldest<T> {
-    inner: Timestamped<T>,
-}
-
-impl<T> PartialOrd for Oldest<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        Some(self.inner.timestamp.cmp(&other.inner.timestamp).reverse())
-    }
-}
-
-impl<T> Ord for Oldest<T> {
-    fn cmp(&self, other: &Self) -> cmp::Ordering {
-        self.inner.timestamp.cmp(&other.inner.timestamp).reverse()
-    }
-}
-
-impl<T> PartialEq for Oldest<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.inner.timestamp.eq(&other.inner.timestamp)
-    }
-}
-
-impl<T> Eq for Oldest<T> {}
-
-fn respond(
-    ptr: &str,
-    cname: &str,
-    listen_addrs: &[SocketAddr],
-    srv: &mut MdnsService,
-    query: Query,
-) {
-    use std::net::SocketAddr::*;
-
-    if query.is_meta_service_query() {
-        let mut packet = PacketBuilder::new();
-        packet.header_mut().set_id(rand::random()).set_query(false);
-        packet.add_answer(ResourceRecord::IN(
-            madness::META_QUERY_SERVICE,
-            RData::ptr(SERVICE_NAME),
-        ));
-        let packet = packet.build();
-        srv.enqueue_response(packet);
-    } else if query.name.as_str() == SERVICE_NAME {
-        let mut packet = PacketBuilder::new();
-        packet.add_answer(ResourceRecord::IN(SERVICE_NAME, RData::ptr(&ptr)));
-
-        for addr in listen_addrs {
-            packet.add_answer(ResourceRecord::IN(
-                &ptr,
-                RData::srv(addr.port(), 0, 0, &cname),
-            ));
-            match addr {
-                V4(ip4) => {
-                    packet.add_answer(
-                        ResourceRecord::IN(&cname, RData::a(*ip4.ip()))
-                            .set_ttl(Duration::from_secs(300)),
-                    );
-                },
-                V6(ip6) => {
-                    packet.add_answer(
-                        ResourceRecord::IN(&cname, RData::aaaa(*ip6.ip()))
-                            .set_ttl(Duration::from_secs(300)),
-                    );
-                },
-            }
-        }
-        packet.header_mut().set_id(rand::random()).set_query(false);
-        let packet = packet.build();
-        srv.enqueue_response(packet);
-    }
-}
-
-fn discover(cname: &str, resp: mdns::Response) -> impl Iterator<Item = (PeerId, Vec<SocketAddr>)> {
-    use mdns::{Record, RecordKind::*};
-
-    let mut ports = BTreeMap::<PeerId, BTreeSet<u16>>::new();
-    let mut addrs = BTreeMap::<PeerId, BTreeSet<IpAddr>>::new();
-    let mut add_addr = |peer, addr| {
-        addrs
-            .entry(peer)
-            .and_modify(|addrs| {
-                addrs.insert(addr);
-            })
-            .or_insert_with(|| vec![addr].into_iter().collect());
-    };
-
-    fn peer_from_domain(dom: &str) -> Option<PeerId> {
-        dom.split('.').next().and_then(|first| first.parse().ok())
-    }
-
-    for Record { name, kind, .. } in resp.records() {
-        match kind {
-            SRV { target, port, .. } if target != cname && name.ends_with(SERVICE_NAME) => {
-                if let Some(peer) = peer_from_domain(target) {
-                    ports
-                        .entry(peer)
-                        .and_modify(|ports| {
-                            ports.insert(*port);
-                        })
-                        .or_insert_with(|| vec![*port].into_iter().collect());
-                }
-            },
-            A(ip4) if name != cname => {
-                if let Some(peer) = peer_from_domain(name) {
-                    add_addr(peer, IpAddr::from(*ip4));
-                }
-            },
-            AAAA(ip6) if name != cname => {
-                if let Some(peer) = peer_from_domain(name) {
-                    add_addr(peer, IpAddr::from(*ip6));
-                }
-            },
-
-            _ => (),
-        }
-    }
-
-    addrs.into_iter().filter_map(move |(peer, addrs)| {
-        ports.get(&peer).map(|ports| {
-            let addrs = addrs
-                .iter()
-                .flat_map(|addr| ports.iter().map(move |port| SocketAddr::new(*addr, *port)))
-                .collect();
-            (peer, addrs)
-        })
-    })
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use pretty_assertions::assert_eq;
-    use std::{collections::HashSet, net::Ipv4Addr};
-
-    use crate::{keys::SecretKey, peer::PeerId};
-
-    // Watch out for MADNESS:
-    //
-    // * `madness` requires the tokio runtime (hence the tokio::test)
-    // * CI (actually: docker) doesn't support multicast UDP (hence the ignore)
-    // * `madness` doesn't support specifying the multicast address or port, so
-    //     * the test must run in an isolated environment
-    //     * we cannot have more than one test function running in parallel
-    #[tokio::test]
-    #[ignore]
-    async fn smoke() {
-        librad_test::logging::init();
-
-        let lolek_id = PeerId::from(SecretKey::new());
-        let lolek_addrs = vec![SocketAddr::new(
-            IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-            666,
-        )];
-        let bolek_id = PeerId::from(SecretKey::new());
-        let bolek_addrs = vec![SocketAddr::new(
-            IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-            777,
-        )];
-        let tola_id = PeerId::from(SecretKey::new());
-        let tola_addrs = vec![SocketAddr::new(
-            IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-            888,
-        )];
-
-        let interval = Duration::from_millis(500);
-        let ttl = Duration::from_secs(60);
-
-        let mut lolek = Mdns::new(lolek_id, lolek_addrs.clone(), interval, ttl).unwrap();
-        let mut bolek = Mdns::new(bolek_id, bolek_addrs.clone(), interval, ttl).unwrap();
-
-        // assert that lolek and bolek discover each other, and nobody else
-        let (lolek_disco, bolek_disco) = futures::join!(lolek.next(), bolek.next());
-        assert_eq!(lolek_disco, Some((bolek_id, bolek_addrs.clone())));
-        assert_eq!(bolek_disco, Some((lolek_id, lolek_addrs.clone())));
-        assert!(bolek.is_pending());
-
-        // assert that a new peer is discovered, while the previous one (still
-        // running) is detected as already seen
-        let _tola = Mdns::new(tola_id, tola_addrs.clone(), interval, ttl).unwrap();
-        let bolek_disco = bolek.next().await;
-        assert_eq!(
-            bolek_disco,
-            Some((tola_id, tola_addrs.clone())),
-            "lolek should be cached"
-        );
-        assert!(bolek.is_pending());
-
-        // assert that, when reset, bolek will see the survivors
-        let mut bolek = Mdns::new(bolek_id, bolek_addrs.clone(), interval, ttl).unwrap();
-        let mut bolek_disco = HashSet::new();
-        for _ in 0..2 {
-            bolek_disco.insert(bolek.next().await.unwrap());
-        }
-        assert_eq!(
-            bolek_disco,
-            [
-                (lolek_id, lolek_addrs.clone()),
-                (tola_id, tola_addrs.clone())
-            ]
-            .iter()
-            .cloned()
-            .collect(),
-            "cache should have been reset"
-        );
-        assert!(bolek.is_pending());
-
-        // assert that the cache ttl is observed by effectively disabling it
-        let mut bolek = Mdns::new(bolek_id, bolek_addrs.clone(), interval, interval).unwrap();
-        for _ in 0..2 {
-            let mut bolek_disco = HashSet::new();
-            for _ in 0..2 {
-                bolek_disco.insert(bolek.next().await.unwrap());
-            }
-            assert_eq!(
-                bolek_disco,
-                [
-                    (lolek_id, lolek_addrs.clone()),
-                    (tola_id, tola_addrs.clone())
-                ]
-                .iter()
-                .cloned()
-                .collect(),
-            );
-            assert!(bolek.is_pending())
-        }
-
-        // assert that advertising new addresses will re-discover that peer
-        let _lolek = Mdns::new(lolek_id, tola_addrs.clone(), interval, ttl).unwrap();
-        let bolek_disco = bolek.next().await;
-        assert_eq!(
-            bolek_disco,
-            Some((lolek_id, tola_addrs.clone())),
-            "lolek should've been determined refreshed"
-        );
-        assert!(bolek.is_pending());
-    }
-}