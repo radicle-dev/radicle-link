6e913ade61049ef285d7b1f27c43bb84d45aed1a
Fintan Halpenny <fintan.halpenny@gmail.com>
2021-04-29 10:46:27 +0100

    smoke: clean up test
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- librad/tests/smoke/history_rewrite.rs
+++ librad/tests/smoke/history_rewrite.rs
@@ -3,38 +3,24 @@
 // This file is part of radicle-link, distributed under the GPLv3 with Radicle
 // Linking Exception. For full terms see the included LICENSE file.
 
-use std::{
-    convert::{TryFrom, TryInto},
-    ops::Index as _,
-};
+use std::ops::Index as _;
 
 use librad::{
     self,
-    git::{
-        local::url::LocalUrl,
-        refs::Refs,
-        storage::Storage,
-        tracking,
-        types::{remote, Flat, Force, GenericRef, Namespace, Reference, Refspec, Remote},
-        Urn,
-    },
+    git::{self, refs::Refs, tracking},
     git_ext as ext,
-    peer::PeerId,
     reflike,
-    refspec_pattern,
 };
 use librad_test::{
-    git::create_commit,
     logging,
     rad::{identities::TestProject, testnet},
 };
-use tokio::task::spawn_blocking;
 
 fn config() -> testnet::Config {
     testnet::Config {
         num_peers: nonzero!(3usize),
-        min_connected: 0,
-        bootstrap: testnet::Bootstrap::None,
+        min_connected: 3,
+        bootstrap: testnet::Bootstrap::from_env(),
     }
 }
 
@@ -55,76 +41,30 @@ async fn sigrefs_dont_get_overwritten() {
     proj.pull(peer1, peer2).await.ok().unwrap();
     proj.pull(peer2, peer3).await.ok().unwrap();
 
-    let default_branch: ext::RefLike = proj
-        .project
-        .doc
-        .payload
-        .subject
-        .default_branch
-        .as_ref()
-        .map(|cstring| cstring.to_string())
-        .unwrap_or_else(|| "mistress".to_owned())
-        .try_into()
+    let commit_urn = proj.project.urn().with_path(reflike!("refs/heads/hi"));
+    peer1
+        .using_storage({
+            let urn = proj.project.urn();
+            let peer = peer2.peer_id();
+            move |storage| -> Result<bool, anyhow::Error> {
+                git::util::quick_commit(
+                    &storage,
+                    &commit_urn,
+                    vec![("README.md", ext::tree::blob(b"Hello, Radicle"))]
+                        .into_iter()
+                        .collect(),
+                    "initial commit",
+                )?;
+                Ok(tracking::track(storage, &urn, peer)?)
+            }
+        })
+        .await
+        .unwrap()
         .unwrap();
 
-    let tmp = tempfile::tempdir().unwrap();
-    let commit_id = spawn_blocking({
-        let urn = proj.project.urn();
-        let owner_subject = proj.owner.subject().clone();
-        let default_branch = default_branch.clone();
-        let peer1 = (*peer1).clone();
-        move || {
-            // Perform commit and push to working copy on peer1
-            let repo = git2::Repository::init(tmp.path().join("peer1")).unwrap();
-            let url = LocalUrl::from(urn.clone());
-            let heads = Reference::heads(Namespace::from(urn), Some(peer1.peer_id()));
-            let remotes = GenericRef::heads(
-                Flat,
-                ext::RefLike::try_from(format!("{}@{}", owner_subject.name, peer1.peer_id()))
-                    .unwrap(),
-            );
-            let mastor = reflike!("refs/heads").join(&default_branch);
-            let mut remote = Remote::rad_remote(
-                url,
-                Refspec {
-                    src: &remotes,
-                    dst: &heads,
-                    force: Force::True,
-                },
-            );
-            let oid = create_commit(&repo, mastor).unwrap();
-            let updated = remote
-                .push(
-                    peer1,
-                    &repo,
-                    remote::LocalPushspec::Matching {
-                        pattern: refspec_pattern!("refs/heads/*"),
-                        force: Force::True,
-                    },
-                )
-                .unwrap()
-                .collect::<Vec<_>>();
-            tracing::debug!("push updated refs: {:?}", updated);
-
-            oid
-        }
-    })
-    .await
-    .unwrap();
-
-    let expected_urn = proj.project.urn().with_path(
-        reflike!("refs/remotes")
-            .join(peer1.peer_id())
-            .join(reflike!("heads"))
-            .join(&default_branch),
-    );
-
-    println!("PULLING PEER 1");
     proj.pull(peer1, peer2).await.unwrap();
-    println!("PULLING PEER 2");
     proj.pull(peer2, peer3).await.unwrap();
 
-    println!("GETTING SIGREFS 2");
     let sigrefs = peer2
         .using_storage({
             let peer = peer1.peer_id();
@@ -135,7 +75,6 @@ async fn sigrefs_dont_get_overwritten() {
         .unwrap()
         .unwrap()
         .unwrap();
-    println!("GETTING SIGREFS 1");
     let expected = peer1
         .using_storage({
             let urn = proj.project.urn();
