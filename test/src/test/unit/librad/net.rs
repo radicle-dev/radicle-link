3b11fd0846a94ff2bf2a7780d9a37e8174f1a563
Kim Altintop <kim@eagain.st>
2021-05-26 20:36:47 +0200

    net: fix CBOR decoder behaviour on incomplete input
    The `futures_codec::Decoder` impl was adapted from the original
    `serde`-based CBOR decoder to `minicbor`. It turns out that this ported
    over a bug: when the input buffer does not contain enough data, its
    offset must **not** be advanced. Otherwise, the next poll will attempt
    to decode from that offset instead of the start of the object, which
    will obviously fail.
    
    Fixes #648
    Signed-off-by: Kim Altintop <kim@eagain.st>

--- test/src/test/unit/librad/net.rs
+++ test/src/test/unit/librad/net.rs
@@ -3,6 +3,7 @@
 // This file is part of radicle-link, distributed under the GPLv3 with Radicle
 // Linking Exception. For full terms see the included LICENSE file.
 
+mod codec;
 mod peer;
 mod protocol;
 mod tls;
