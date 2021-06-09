67ec3d093f696a02ee60c5d42b4d79d529b9478e
Kim Altintop <kim@eagain.st>
2021-06-09 15:23:33 +0200

    fixup! create reflogs explicitly to reduce IO

--- librad/src/git/storage/watch.rs
+++ librad/src/git/storage/watch.rs
@@ -40,6 +40,12 @@ pub struct NamespaceEvent {
     pub kind: EventKind,
 }
 
+#[derive(Debug)]
+pub struct ReflogEvent {
+    pub path: PathBuf,
+    pub kind: EventKind,
+}
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 #[non_exhaustive]
 pub enum EventKind {
@@ -54,6 +60,32 @@ pub struct Watch<'a> {
 
 impl<'a> Watch<'a> {
     pub fn namespaces(&self) -> Result<(Watcher, impl Iterator<Item = NamespaceEvent>), Error> {
+        fn is_namespace(p: &Path) -> bool {
+            let mut iter = p.iter().take(6);
+            iter.next() == Some("refs".as_ref())
+                && iter.next() == Some("namespaces".as_ref())
+                && iter.next().is_some()
+                && iter.next() == Some("rad".as_ref())
+                && iter.next() == Some("id".as_ref())
+                && iter.next().is_none()
+        }
+
+        let (watcher, rx) = self.reflogs()?;
+        let rx = rx.filter_map(move |ReflogEvent { path, kind }| {
+            if matches!(kind, EventKind::Create | EventKind::Remove) {
+                is_namespace(&path).then(|| NamespaceEvent {
+                    path: path.iter().take(3).collect(),
+                    kind,
+                })
+            } else {
+                None
+            }
+        });
+
+        Ok((watcher, rx))
+    }
+
+    pub fn reflogs(&self) -> Result<(Watcher, impl Iterator<Item = ReflogEvent>), Error> {
         use notify::{DebouncedEvent::*, RecursiveMode::Recursive};
 
         let repo_path = self.storage.path().to_owned();
@@ -68,25 +100,28 @@ impl<'a> Watch<'a> {
         let mut watcher = notify::watcher(tx, DEBOUNCE_DELAY)?;
         watcher.watch(&reflogs_path, Recursive)?;
 
-        fn is_namespace(p: &Path) -> bool {
-            p.starts_with("refs/namespaces") && p.iter().take(4).count() == 3
-        }
-
         let rx = rx.into_iter().filter_map(move |evt| {
             tracing::trace!("reflog event: {:?}", evt);
             match evt {
                 Create(path) => {
                     let path = path.strip_prefix(&reflogs_path).ok()?;
-                    is_namespace(path).then(|| NamespaceEvent {
+                    path.is_file().then(|| ReflogEvent {
                         path: path.to_path_buf(),
                         kind: EventKind::Create,
                     })
                 },
                 Remove(path) => {
                     let path = path.strip_prefix(&reflogs_path).ok()?;
-                    is_namespace(path).then(|| NamespaceEvent {
+                    path.is_file().then(|| ReflogEvent {
                         path: path.to_path_buf(),
                         kind: EventKind::Remove,
+                    })
+                },
+                Write(path) | Rename(_, path) => {
+                    let path = path.strip_prefix(&reflogs_path).ok()?;
+                    path.is_file().then(|| ReflogEvent {
+                        path: path.to_path_buf(),
+                        kind: EventKind::Update,
                     })
                 },
                 _ => None,
