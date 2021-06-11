68a6d0fed88c979b214694d656bc656306245def
Kim Altintop <kim@eagain.st>
2021-06-11 15:52:31 +0200

    fixup! test: use RefLike conversion from Path

--- test/src/test/unit/librad/git/storage/watch.rs
+++ test/src/test/unit/librad/git/storage/watch.rs
@@ -3,7 +3,7 @@
 // This file is part of radicle-link, distributed under the GPLv3 with Radicle
 // Linking Exception. For full terms see the included LICENSE file.
 
-use std::{collections::BTreeSet, convert::TryFrom as _, thread, time::Duration};
+use std::{collections::BTreeSet, convert::TryFrom as _, time::Duration};
 
 use librad::{
     git::{
@@ -27,28 +27,24 @@ fn namespaces() {
     let (watcher, events) = store.watch().namespaces(DEBOUNCE_DELAY).unwrap();
     let TestProject { project, owner } = TestProject::create(&store).unwrap();
 
-    thread::sleep(DEBOUNCE_DELAY);
-    drop(watcher);
+    let expected = vec![
+        (project.urn(), EventKind::Create),
+        (owner.urn(), EventKind::Create),
+    ]
+    .into_iter()
+    .collect::<BTreeSet<_>>();
 
     let events = events
-        .into_iter()
+        .take(expected.len())
         .map(|NamespaceEvent { path, kind }| {
-            (
-                Urn::try_from(RefLike::try_from(path.as_path()).unwrap()).unwrap(),
-                kind,
-            )
+            let refl = RefLike::try_from(path.as_path()).unwrap();
+            let urn = Urn::try_from(refl).unwrap();
+            (urn, kind)
         })
         .collect::<BTreeSet<_>>();
+    drop(watcher);
 
-    assert_eq!(
-        vec![
-            (project.urn(), EventKind::Create),
-            (owner.urn(), EventKind::Create)
-        ]
-        .into_iter()
-        .collect::<BTreeSet<_>>(),
-        events
-    )
+    assert_eq!(expected, events)
 }
 
 #[test]
@@ -59,46 +55,42 @@ fn reflogs() {
     let (watcher, events) = store.watch().reflogs(DEBOUNCE_DELAY).unwrap();
     let TestProject { project, owner } = TestProject::create(&store).unwrap();
 
-    thread::sleep(DEBOUNCE_DELAY);
-    drop(watcher);
+    let expected = vec![
+        (
+            project.urn().with_path(reflike!("refs/rad/id")),
+            EventKind::Create,
+        ),
+        (
+            project.urn().with_path(reflike!("refs/rad/self")),
+            EventKind::Create,
+        ),
+        (
+            project
+                .urn()
+                .with_path(reflike!("refs/rad/ids").join(owner.urn())),
+            EventKind::Create,
+        ),
+        (
+            owner.urn().with_path(reflike!("refs/rad/id")),
+            EventKind::Create,
+        ),
+        (
+            owner.urn().with_path(reflike!("refs/rad/self")),
+            EventKind::Create,
+        ),
+    ]
+    .into_iter()
+    .collect::<BTreeSet<_>>();
 
     let events = events
-        .into_iter()
+        .take(expected.len())
         .map(|ReflogEvent { path, kind }| {
-            (
-                Urn::try_from(RefLike::try_from(path.as_path()).unwrap()).unwrap(),
-                kind,
-            )
+            let refl = RefLike::try_from(path.as_path()).unwrap();
+            let urn = Urn::try_from(refl).unwrap();
+            (urn, kind)
         })
         .collect::<BTreeSet<_>>();
+    drop(watcher);
 
-    assert_eq!(
-        vec![
-            (
-                project.urn().with_path(reflike!("refs/rad/id")),
-                EventKind::Create
-            ),
-            (
-                project.urn().with_path(reflike!("refs/rad/self")),
-                EventKind::Create
-            ),
-            (
-                project
-                    .urn()
-                    .with_path(reflike!("refs/rad/ids").join(owner.urn())),
-                EventKind::Create
-            ),
-            (
-                owner.urn().with_path(reflike!("refs/rad/id")),
-                EventKind::Create
-            ),
-            (
-                owner.urn().with_path(reflike!("refs/rad/self")),
-                EventKind::Create
-            )
-        ]
-        .into_iter()
-        .collect::<BTreeSet<_>>(),
-        events
-    )
+    assert_eq!(expected, events)
 }
