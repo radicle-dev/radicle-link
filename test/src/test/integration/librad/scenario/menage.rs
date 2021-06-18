cc2df1a0cdbfa8eb469b8b72a57c74bb4948270b
Kim Altintop <kim@eagain.st>
2021-06-18 08:34:16 +0200

    test: ignore menage tests
    We know it's broken, right?
    
    Signed-off-by: Kim Altintop <kim@eagain.st>

--- test/src/test/integration/librad/scenario/menage.rs
+++ test/src/test/integration/librad/scenario/menage.rs
@@ -71,6 +71,7 @@ impl ExpectedReferences {
 }
 
 #[test]
+#[ignore]
 fn a_trois() {
     logging::init();
 
@@ -217,6 +218,7 @@ fn a_trois() {
 /// When `peer3` replicates from `peer1` they should have references for `peer1`
 /// and `peer2`, due to the tracking graph.
 #[test]
+#[ignore]
 fn threes_a_crowd() {
     logging::init();
 
