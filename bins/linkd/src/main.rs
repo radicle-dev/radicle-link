1e0dd589f986cf978ee7bdc2bcf4c9c291a3fb2b
Alexander Simmerl <a.simmerl@gmail.com>
2021-09-14 23:34:10 +1000

    linkd: Add binary
    A very thin and light wrapper around the `node::run` exposed from the
    node lib crate, so `linkd` exists as a deploy target.
    
    Bootstrapping a whole new workspace which is excluded from root, allows
    for a checked in `Cargo.lock` without affecting the lib crates.
    Historically this used to be the role of radicle-bins.
    
    Signed-off-by: Alexander Simmerl <a.simmerl@gmail.com>

---
+++ bins/linkd/src/main.rs
@@ -0,0 +1,13 @@
+// Copyright © 2021 The Radicle Link Contributors
+//
+// This file is part of radicle-link, distributed under the GPLv3 with Radicle
+// Linking Exception. For full terms see the included LICENSE file.
+
+use node_lib::node::run;
+
+#[tokio::main]
+async fn main() {
+    if let Err(e) = run().await {
+        eprintln!("linkd failed: {:?}", e);
+    }
+}
