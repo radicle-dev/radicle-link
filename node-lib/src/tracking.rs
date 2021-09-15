63ae6d44659942fa2ab48fe91aaa10ba3fd93847
xla <self@xla.is>
2021-09-15 16:01:02 +1000

    node: Implement tracking
    Adds a new subroutine to the node library which takes care of tracking
    projects based on a pre-defined set of either everything, certain Urns,
    anything from a certain peers, or mix of the latter two. This is a port
    of the tracking code that currently exists in the seed lib and will be
    exposed as configuration in a follow commit.
    
    Signed-off-by: xla <sefl@xla.is>

---
+++ node-lib/src/tracking.rs
@@ -0,0 +1,99 @@
+// Copyright © 2021 The Radicle Link Contributors
+//
+// This file is part of radicle-link, distributed under the GPLv3 with Radicle
+// Linking Exception. For full terms see the included LICENSE file.
+
+use std::collections::HashSet;
+
+use futures::{pin_mut, StreamExt as _};
+use tracing::{error, info, instrument};
+
+use librad::{
+    git::{replication, storage::fetcher, tracking, Urn},
+    net::{
+        peer::{event::upstream::Gossip, Peer, PeerInfo, ProtocolEvent},
+        protocol::{broadcast::PutResult::Uninteresting, gossip::Payload},
+    },
+    PeerId,
+    Signer,
+};
+
+pub enum Tracker {
+    Everything,
+    Selected {
+        peer_ids: HashSet<PeerId>,
+        urns: HashSet<Urn>,
+    },
+}
+
+impl Tracker {
+    fn is_tracked(&self, peer_id: &PeerId, urn: &Urn) -> bool {
+        match self {
+            Tracker::Everything => true,
+            Tracker::Selected {
+                ref peer_ids,
+                ref urns,
+            } if peer_ids.contains(peer_id) || urns.contains(urn) => true,
+            _ => false,
+        }
+    }
+}
+
+#[instrument(name = "tracking subroutine", skip(peer, tracker))]
+pub async fn routine<D, S>(peer: Peer<S>, tracker: Tracker) -> anyhow::Result<()>
+where
+    S: Signer + Clone,
+{
+    let replication_cfg = peer.protocol_config().replication;
+    let events = peer.subscribe();
+    pin_mut!(events);
+
+    while let Some(res) = events.next().await {
+        if let Err(err) = res {
+            error!(?err, "event error");
+            continue;
+        }
+
+        if let ProtocolEvent::Gossip(box Gossip::Put {
+            payload: Payload { urn, .. },
+            provider:
+                PeerInfo {
+                    peer_id,
+                    seen_addrs,
+                    ..
+                },
+            result: Uninteresting,
+        }) = res.unwrap()
+        {
+            if !tracker.is_tracked(&peer_id, &urn) {
+                continue;
+            }
+
+            let res = {
+                let urn = urn.clone();
+                peer.using_storage(move |storage| {
+                    let updated = tracking::track(storage, &urn, peer_id)?;
+
+                    // Skip explicit replication if the peer is already tracked.
+                    if updated {
+                        let addr_hints = seen_addrs.iter().copied().collect::<Vec<_>>();
+                        let fetcher = fetcher::PeerToPeer::new(urn.clone(), peer_id, addr_hints)
+                            .build(storage)??;
+                        replication::replicate(storage, fetcher, replication_cfg, None)?;
+                    }
+
+                    Ok::<_, anyhow::Error>(updated)
+                })
+                .await?
+            };
+
+            match res {
+                Ok(true) => info!("tracked project {} from {}", urn, peer_id),
+                Ok(false) => info!("already tracked {} from {}", urn, peer_id),
+                Err(err) => error!(?err, "tracking failed for {} from {}", urn, peer_id),
+            }
+        }
+    }
+
+    Ok(())
+}
