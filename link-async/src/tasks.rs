56aae1a3695b2ab01b559c033330b1556529fc55
Kim Altintop <kim@eagain.st>
2021-12-09 08:16:25 +0100

    link-async: remove foundation copyright notice
    Signed-off-by: Kim Altintop <kim@eagain.st>

--- link-async/src/tasks.rs
+++ link-async/src/tasks.rs
@@ -1,4 +1,3 @@
-// Copyright © 2021 The Radicle Foundation <hello@radicle.foundation>
 // Copyright © 2021 The Radicle Link Contributors
 //
 // This file is part of radicle-link, distributed under the GPLv3 with Radicle
