b71b994feb79564c46951349a21eb7c6f85daf35
Fintan Halpenny <fintan.halpenny@gmail.com>
2022-02-24 09:16:36 +0000

    ci(macos): fix imports
    The macos build was failing due to unused imports. This patch
    rearranges or removes the imports where needed.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- node-lib/src/api/sockets/socket_activation/macos.rs
+++ node-lib/src/api/sockets/socket_activation/macos.rs
@@ -3,12 +3,10 @@
 // This file is part of radicle-link, distributed under the GPLv3 with Radicle
 // Linking Exception. For full terms see the included LICENSE file.
 
-use std::os::unix::net::UnixListener;
-
 use anyhow::Result;
 
-use super::Sockets;
+use super::SyncSockets;
 
-pub fn env() -> Result<Option<Sockets>> {
+pub fn env() -> Result<Option<SyncSockets>> {
     todo!()
 }
