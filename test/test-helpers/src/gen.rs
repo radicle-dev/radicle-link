3ce05309a1353de6f1e64c8d027cd4be6dc95739
Fintan Halpenny <fintan.halpenny@gmail.com>
2022-03-24 16:24:04 +0000

    test: add SocketAddr generators
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- test/test-helpers/src/gen.rs
+++ test/test-helpers/src/gen.rs
@@ -1,7 +1,4 @@
 // Copyright © 2022 The Radicle Link Contributors
 // SPDX-License-Identifier: GPLv3-or-later
 
-pub mod gen;
-pub mod logging;
-pub mod roundtrip;
-pub mod tempdir;
+pub mod std_net;
