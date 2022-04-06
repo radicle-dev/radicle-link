c46a21b847147bc72120aff26e1d91374c35a492
Fintan Halpenny <fintan.halpenny@gmail.com>
2022-04-06 10:03:43 +0100

    protocol: remove State param from recv::git
    The recv::git portion of the protocol is required by the client that
    will not take part in the gossip network. Thus, it is necessary to not
    pass the `protocol::State` to the `git` function call.
    
    Make the `git` function call only require the `Paths` parameter,
    ie. removing `protocol::State`.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- librad/src/net/protocol/io/recv/git.rs
+++ librad/src/net/protocol/io/recv/git.rs
@@ -11,10 +11,12 @@ use link_git::protocol::upload_pack::{upload_pack, Header};
 use thiserror::Error;
 use tracing::{error, info};
 
-use crate::net::{
-    connection::Duplex,
-    protocol::State,
-    upgrade::{self, Upgraded},
+use crate::{
+    net::{
+        connection::Duplex,
+        upgrade::{self, Upgraded},
+    },
+    paths::Paths,
 };
 
 #[derive(Debug, Error)]
@@ -26,27 +28,25 @@ enum Error {
     Io(#[from] io::Error),
 }
 
-pub(in crate::net::protocol) async fn git<S, G, T>(
-    state: &State<S, G>,
-    stream: Upgraded<upgrade::Git, T>,
-) where
+pub(in crate::net::protocol) async fn git<T>(paths: &Paths, stream: Upgraded<upgrade::Git, T>)
+where
     T: Duplex,
     T::Read: AsyncRead + Unpin,
     T::Write: AsyncWrite + Unpin,
 {
-    if let Err(e) = serve(state, stream).await {
+    if let Err(e) = serve(paths, stream).await {
         error!(err = ?e, "upload-pack error");
     }
 }
 
-async fn serve<S, G, T>(state: &State<S, G>, stream: Upgraded<upgrade::Git, T>) -> Result<(), Error>
+async fn serve<T>(paths: &Paths, stream: Upgraded<upgrade::Git, T>) -> Result<(), Error>
 where
     T: Duplex,
     T::Read: AsyncRead + Unpin,
     T::Write: AsyncWrite + Unpin,
 {
     let (recv, send) = stream.into_stream().split();
-    let git_dir = state.config.paths.git_dir();
+    let git_dir = paths.git_dir();
 
     let (Header { path, host, extra }, run) = upload_pack(git_dir, recv, send).await?;
     info!(%path, ?host, ?extra, "upload-pack");
